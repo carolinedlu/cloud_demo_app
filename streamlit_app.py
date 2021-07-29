6ec7b3e9b4b8737a3efb3a9405ac920a14b814ad
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-29 08:45:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.balloons()
-st.markdown('**Hello Witty Works team!**')
+st.markdown('**Hello BuildNN team!**')
 st.markdown('We are very excited to be working with you.')
