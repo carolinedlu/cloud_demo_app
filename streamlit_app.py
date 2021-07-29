703933106d11ab1d3ab265fa48ac1690518df1c4
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-29 11:30:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.balloons()
-st.markdown('**Hello BuildNN team!**')
+st.markdown('**Hello Institute for Communicative and Congnitive Neurosciences!**')
 st.markdown('We are very excited to be working with you.')
